653c86862917cbfd311d227d703cc74a1285ba4c
sashuutakap837 <91487208+sashuutakap837@users.noreply.github.com>
2022-02-19 16:40:58 +0300

    Add files via upload

---
+++ task2/src/task2/Main.java
@@ -0,0 +1,11 @@
+package task2;
+
+public class Main {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+		System.out.println("Вы не передавали параметров");
+		} else {
+		System.out.println("Вы ввели " + args.length + " параметров");
+		}
+		}
+}
