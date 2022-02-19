90db5e59a40ec5791799641d16340845a46601c4
sashuutakap837 <91487208+sashuutakap837@users.noreply.github.com>
2022-02-19 16:40:39 +0300

    Add files via upload

---
+++ task1/src/task1/Main.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class Main {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+		System.out.println("Hello World");
+		} else {
+		System.out.println("Hello " + args[0]);
+		}
+		}
+}
