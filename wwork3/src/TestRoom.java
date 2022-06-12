3eda505bfd3376f333cd9ae54b4a98750465f055
sashuutakap837 <91487208+sashuutakap837@users.noreply.github.com>
2022-06-12 19:29:37 +0300

    Add files via upload

---
+++ wwork3/src/TestRoom.java
@@ -0,0 +1,10 @@
+
+public class TestRoom {
+	public class TestRoom {
+		public static void main(String[] args){
+		Room room=new Room(017, Мирошникова, Золотой луг, 5, 1, 01.06, 22.06, 1, 5600);
+		System.out.println(room.toString());
+
+		}
+		}
+}
---
+++ wwork3/src/module-info.java
@@ -0,0 +1,2 @@
+module wwork3 {
+}
